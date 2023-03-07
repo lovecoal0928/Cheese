aeed9d56b5270e3e2b0f07c15adea2fa562ecc33
hiroto2002 <hiroto2002h14@icloud.com>
2023-03-07 17:34:56 +0900

    feat:#1

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -3,6 +3,7 @@ import { ComponentProps } from 'react'
 type Button = {
   label: string
 } & ComponentProps<'button'>
+
 export const Button = ({ label }: Button) => {
   return <button>{label}</button>
 }
