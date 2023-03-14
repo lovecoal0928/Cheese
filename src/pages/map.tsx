5617c932e28c8741cd15af1ac89f5bf3d11fd220
NewNomad <tosiki1997@gmail.com>
2023-03-14 19:01:54 +0900

    add: google map

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -1,8 +1,59 @@
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
 import { NextPage } from "next";
-import React from "react";
+import React, { useCallback, useState } from "react";
+
+const center = {
+  lat: -3.745,
+  lng: -38.523
+}
 
 const map: NextPage = () => {
-  return <div>login</div>;
+  const APIkey = process.env.GCP_KEY;
+  // console.log(process.env.GCP_KE);
+
+  if (!APIkey) return <div>APIキーが登録されていません</div>
+
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: APIkey
+  })
+
+  const [map, setMap] = useState(null);
+
+  const onLoad = useCallback(
+    (map: any) => {
+      const bounds = new window.google.maps.LatLngBounds(center)
+      map.fitBounds(bounds)
+      setMap(map);
+    },
+    [],
+  )
+
+  const onUnmount = useCallback(
+    (map: any) => {
+      setMap(null);
+    },
+    [],
+  )
+
+
+  return (
+    <>
+      {isLoaded ?
+        <GoogleMap
+          center={center}
+          zoom={10}
+          onLoad={onLoad}
+          onUnmount={onUnmount}
+        >
+
+        </GoogleMap>
+        : <></>
+      }
+
+
+    </>
+  )
 };
 
 export default map
