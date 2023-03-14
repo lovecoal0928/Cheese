55ce15a3bffdc0bdc2a9f78c2b70297a8c8233b4
NewNomad <tosiki1997@gmail.com>
2023-03-14 19:34:54 +0900

    fix: Next.js用に最適化

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -1,4 +1,4 @@
-import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import { GoogleMap, LoadScriptNext, useJsApiLoader } from "@react-google-maps/api";
 import { NextPage } from "next";
 import React, { useCallback, useState } from "react";
 
@@ -8,51 +8,28 @@ const center = {
 }
 
 const map: NextPage = () => {
-  const APIkey = process.env.GCP_KEY;
-  // console.log(process.env.GCP_KE);
+  const APIkey = "AIzaSyCw2xTxfg_-I5lbXu6XqR3vATpm-GV_jog"
 
-  if (!APIkey) return <div>APIキーが登録されていません</div>
-
-  const { isLoaded } = useJsApiLoader({
-    id: "google-map-script",
-    googleMapsApiKey: APIkey
-  })
-
-  const [map, setMap] = useState(null);
-
-  const onLoad = useCallback(
-    (map: any) => {
-      const bounds = new window.google.maps.LatLngBounds(center)
-      map.fitBounds(bounds)
-      setMap(map);
-    },
-    [],
-  )
-
-  const onUnmount = useCallback(
-    (map: any) => {
-      setMap(null);
-    },
-    [],
-  )
-
+  const center = {
+    lat: -3.745,
+    lng: -38.523
+  }
 
   return (
-    <>
-      {isLoaded ?
-        <GoogleMap
-          center={center}
-          zoom={10}
-          onLoad={onLoad}
-          onUnmount={onUnmount}
-        >
+    <LoadScriptNext googleMapsApiKey={APIkey}>
+      <GoogleMap
+        mapContainerStyle={{
+          width: "400px",
+          height: "400px",
+        }}
+        center={center}
+        zoom={10}
+      >
 
-        </GoogleMap>
-        : <></>
-      }
+      </GoogleMap>
 
 
-    </>
+    </LoadScriptNext>
   )
 };
 
