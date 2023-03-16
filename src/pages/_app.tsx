cf3ba5f18bde556fbcb7342405c6c974ab16c286
yudai2929 <yudai201729@icloud.com>
2023-03-16 17:36:19 +0900

    ✨feat: setup react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
-import { NextPage } from "next";
-import { AppProps } from "next/app";
+import { QueryClientProvider } from '@tanstack/react-query'
+import { NextPage } from 'next'
+import { AppProps } from 'next/app'
+import { queryClient } from 'utils/tanStack'
 
 const MyApp: NextPage<AppProps> = ({ Component, pageProps }: AppProps) => {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
-    </>
-  );
-};
+    </QueryClientProvider>
+  )
+}
 
-export default MyApp;
+export default MyApp
