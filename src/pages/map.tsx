dfbd9751ae607089fcbc833a2c069fa7dd0a4468
NewNomad <tosiki1997@gmail.com>
2023-03-18 01:56:06 +0900

    add: DirectionService, DirectionRenderer

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -1,4 +1,4 @@
-import { GoogleMap, LoadScriptNext, Marker, MarkerF, useJsApiLoader } from "@react-google-maps/api";
+import { DirectionsRenderer, DirectionsService, GoogleMap, GoogleMapProps, LoadScriptNext, Marker, MarkerF, } from "@react-google-maps/api";
 import { NextPage } from "next";
 import React, { useCallback, useEffect, useState } from "react";
 
@@ -22,10 +22,27 @@ const locates = [
   },
 ]
 
+const origin = {
+  lat: 34.933249,
+  lng: 137.168636,
+}
+
+const destination = {
+  lat: 34.93145,
+  lng: 137.16265,
+}
+
+const transpoints = [
+  {
+    location: {
+      lat: 34.93876,
+      lng: 137.16650,
+    }
+  }
+]
 const map: NextPage = () => {
   const [center, setCenter] = useState({ lat: 0, lng: 0 });
-
-
+  // 現在位置を取得
   useEffect(() => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
@@ -36,6 +53,24 @@ const map: NextPage = () => {
       });
     }
   }, []);
+
+  const [currentDirection, setCurrentDirection] = useState(null)
+
+  const directionsCallback = useCallback((googleRes: any) => {
+    if (googleRes) {
+      if (currentDirection) {
+        if (googleRes.status === "OK" &&
+          googleRes.geocoded_waypoints.length
+        ) {
+          setCurrentDirection(googleRes)
+        }
+      } else {
+        if (googleRes.status === "OK") {
+          setCurrentDirection(googleRes)
+        }
+      }
+    }
+  }, [])
 
   return (
     <LoadScriptNext googleMapsApiKey={APIkey}>
@@ -57,6 +92,21 @@ const map: NextPage = () => {
           <MarkerF position={locate} key={i} />
         ))}
         <MarkerF position={center} />
+        <DirectionsService
+          options={{
+            origin: origin,
+            destination,
+            travelMode: google.maps.TravelMode.WALKING,
+            optimizeWaypoints: true,
+            waypoints: transpoints
+          }}
+          callback={directionsCallback}
+        />
+        {currentDirection !== null && (
+          <DirectionsRenderer
+            options={{ directions: currentDirection }}
+          />
+        )}
       </GoogleMap>
 
 
