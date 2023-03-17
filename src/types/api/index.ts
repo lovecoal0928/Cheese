5b5ac4f7f36b5530b56016848eb203e9e40d7b07
yudai2929 <yudai201729@icloud.com>
2023-03-17 11:14:49 +0900

    🐛fix: build error

--- src/types/api/index.ts
+++ src/types/api/index.ts
@@ -1,5 +1,5 @@
 import { Tables } from 'plugins/supabse'
-import { SnakeToCamel } from 'repositories/converters'
+import { SnakeToCamel } from 'types/converters'
 
 export type UserParams = SnakeToCamel<Tables['users']['Insert']>
 export type UpdateUserParams = SnakeToCamel<Tables['users']['Update']>
