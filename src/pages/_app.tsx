c3525b65df97e844d9f2b755f6d8215e4d4ddb18
yudai2929 <yudai201729@icloud.com>
2023-03-17 10:57:23 +0900

    ✨feat: 新規ユーザー登録機能を実装

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import { QueryClientProvider } from '@tanstack/react-query'
 import { NextPage } from 'next'
 import { AppProps } from 'next/app'
+import { AuthProvider } from 'providers/authProvider'
 import { queryClient } from 'utils/tanStack'
 
 const MyApp: NextPage<AppProps> = ({ Component, pageProps }: AppProps) => {
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
     </QueryClientProvider>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import { NextPage } from 'next'
-import { Graph } from 'types/';
-import { getShortestPath } from 'utils/hooks/dijkstra';
+import { Graph } from 'types/'
+import { getShortestPath } from 'utils/hooks/dijkstra'
+import { useAuth } from 'utils/hooks/useAuth'
 
 const Home: NextPage = () => {
+  const { signIn } = useAuth()
 
   // グラフの定義
   const graph: Graph = {
@@ -11,19 +13,18 @@ const Home: NextPage = () => {
     C: { A: 1, B: 2, D: 4, E: 8, F: 3 },
     D: { A: 6, B: 1, C: 4, E: 3, F: 9 },
     E: { A: 8, B: 5, C: 8, D: 3, F: 2 },
-    F: { A: 4, B: 7, C: 3, D: 9, E: 2 }
-  };
+    F: { A: 4, B: 7, C: 3, D: 9, E: 2 },
+  }
 
   // 実行例
-  const a = getShortestPath(graph);
-  console.log(a); // { A: 0, B: 5, C: 1, D: 6, E: 9, F: 4 }
+  const a = getShortestPath(graph)
+  // console.log(a)
+  // { A: 0, B: 5, C: 1, D: 6, E: 9, F: 4 }
   // console.log(previous); // { A: null, B: 'A', C: 'A', D: 'A', E: 'C', F: 'A' }
-
-
 
   return (
     <>
-      <div></div>
+      <button onClick={signIn}>signup</button>
     </>
   )
 }
