e6d628b9eb136fb18f305fb60207e4214013a93a
yudai2929 <yudai201729@icloud.com>
2023-03-18 16:59:06 +0900

    👍feat: post hooks を作成

--- src/pages/post.tsx
+++ src/pages/post.tsx
@@ -1,8 +1,19 @@
 import { NextPage } from 'next'
 import React from 'react'
 import { Post } from '@/components/templates/Post'
+import { useFetchPosts } from 'utils/hooks/post/useFetchPost'
 
 const post: NextPage = () => {
+  const {
+    data: posts,
+    refetch: refetchPosts,
+    isLoading: isFetchPostLoading,
+  } = useFetchPosts()
+
+  React.useEffect(() => {
+    console.log(posts)
+  }, [posts])
+
   return <Post />
 }
 
