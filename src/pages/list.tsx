70a2d5ee21653bc9f574355f73b3a00a78fb0426
hiroto2002 <hiroto2002h14@icloud.com>
2023-03-18 18:28:05 +0900

    fix:type

--- src/pages/list.tsx
+++ src/pages/list.tsx
@@ -3,7 +3,33 @@ import React from 'react'
 import { List } from '@/components/templates/List'
 
 const list: NextPage = () => {
-  const data = [{ title: '', address: '', src: '' }]
+  const data = [
+    {
+      postId: '',
+      userId: '',
+      title: 'タイトル',
+      comment: 'コメント',
+      postedAt: '',
+      updatedAt: '',
+      postImages: [
+        {
+          postImageId: '',
+          imagePath: '',
+          imageTags: [
+            {
+              tagId: '',
+              name: 'タグ',
+            },
+          ],
+        },
+      ],
+      address: {
+        addressesId: '',
+        longitude: 10,
+        latitude: 20,
+      },
+    },
+  ]
   return <List data={data} />
 }
 
