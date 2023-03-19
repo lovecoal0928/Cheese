202d610ca1b88544faabce659f2492e3b0cd16f5
NewNomad <tosiki1997@gmail.com>
2023-03-19 16:29:40 +0900

    add: reset.css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import { NextPage } from 'next'
 import { AppProps } from 'next/app'
 import { AuthProvider } from 'providers/authProvider'
 import { queryClient } from 'utils/tanStack'
-import "modern-css-reset/dist/reset.min.css"
+import "../styles/reset.css"
 
 const MyApp: NextPage<AppProps> = ({ Component, pageProps }: AppProps) => {
   return (
